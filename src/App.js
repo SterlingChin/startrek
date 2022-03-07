16d1c8a1a4a6bcc7e6e86ac2008173dfba983efe
Sterling Chin <sterling.chinpostman.com@Sterlings-MBP.attlocal.net>
2022-03-06 21:22:48 -0600

    initial set of data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {ep} from './Star Trek episodes-ranked'
+import styled from 'styled-components';
+
+const ListItem = styled('li')`
+  list-style-type: none;
+  padding: 0;
+  margin: 0
+`
 
 function App() {
+  console.log(ep)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button></button>
+      <ul>
+        {ep.map(x =>{ 
+          return (
+            <ListItem>
+              <input type='checkbox'/>
+                <label>{x}</label>
+            </ListItem>
+        )}
+        )}
+      </ul>
     </div>
   );
 }
